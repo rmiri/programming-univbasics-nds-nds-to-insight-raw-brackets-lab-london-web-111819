d1533de6455a6d6599ddf523e7497ecf752cf3c8
Renata Freitas <r.miriuk@gmail.com>
2019-10-22 21:45:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,14 +9,19 @@ def directors_totals(nds)
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
-  #
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
   # Be sure to return the result at the end!
+
+  #nds[directors_row][:name][:movies][movies_collumn][:worldwide_gross]
+  totes = 0
+
+  nds.each do |n|
+    n[1].each do |y|
+      totes += y[1].inspect.to_i
+    end
+    puts "the #{n[0]} has #{totes}"
+  end
+
   nil
 end
